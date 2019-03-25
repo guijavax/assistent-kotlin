35a9f4594c6b1a771d5a721cdd3c026ea9780ef9
Guilherme <you@example.com>
2019-03-24 21:30:59 -0300

    first commit

---
+++ src/main/kotlin/br/com/assistent/assistentKotlin/AssistentKotlinApplication.kt
@@ -0,0 +1,11 @@
+package br.com.assistent.assistentKotlin
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AssistentKotlinApplication
+
+fun main(args: Array<String>) {
+	SpringApplication.run(AssistentKotlinApplication::class.java, *args)
+}
