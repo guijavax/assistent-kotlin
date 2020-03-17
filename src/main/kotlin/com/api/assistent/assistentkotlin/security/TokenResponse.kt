7240d8cbbb91ff884bce24e3d1e56cc5dff20770
Guilherme <guilhermeborgesti@gmail.com>
2020-03-16 23:21:20 -0300

    refatoracao da camada se segurança pt1 - exclusao dos arquivos

--- src/main/kotlin/com/api/assistent/assistentkotlin/security/TokenResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.api.assistent.assistentkotlin.security
-
-data class TokenResponse (
-        var userId: String = "",
-        var userName : String = "",
-        var token : String = "",
-        var isFirstLogin : Boolean = false){}
