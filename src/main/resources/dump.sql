90036b4b1853706a383ef8f4fef9bb1cd31b9d6d
Guilherme <guilhermeborgesti@gmail.com>
2020-04-02 12:03:25 -0300

    alteracao dump

--- src/main/resources/dump.sql
+++ src/main/resources/dump.sql
@@ -346,7 +346,7 @@ CREATE TABLE public.type_product (
 	CONSTRAINT type_product_pkey PRIMARY KEY (id_type)
 );
 
-ALTER TABLE public.products ADD null;
+alter table products add foreign key (id_type_product) references type_product (id_type) on delete cascade on update cascade;
 
 --
 -- TOC entry 2861 (class 0 OID 16405)
